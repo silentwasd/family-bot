7f854342756051fda379e78f605683a95c8fb61c
silentwasd <silentwasd@gmail.com>
2021-05-28 22:00:20 +0500

    Более сложное предложение.

--- app/Services/Telegram/UpdateHandlers/Fun/MakeSentence.php
+++ app/Services/Telegram/UpdateHandlers/Fun/MakeSentence.php
@@ -9,6 +9,7 @@ use App\Models\Lang\Noun;
 use App\Models\Lang\Verb;
 use App\Services\Lang\Word;
 use App\Services\Telegram\UpdateHandlers\MatchHandler;
+use Illuminate\Support\Str;
 use function Symfony\Component\String\s;
 
 class MakeSentence extends MatchHandler
@@ -17,19 +18,51 @@ class MakeSentence extends MatchHandler
 
     protected function matched(array $matches = []): string
     {
-        $nounA = (new Word( Noun::query()->inRandomOrder()->first() ))->noun();
-        $adjA = (new Word( Adjective::query()->inRandomOrder()->first() ))->adjective();
-        $verb = (new Word( Verb::query()->inRandomOrder()->first() ))->verb();
-        $nounB = (new Word( Noun::query()->inRandomOrder()->first() ))->noun();
-        $adjB = (new Word( Adjective::query()->inRandomOrder()->first() ))->adjective();
+        $nouns = Noun::query()
+            ->inRandomOrder()
+            ->take(3)
+            ->get()
+            ->map(function ($item) {
+                return (new Word($item))->noun();
+            })
+            ->all();
 
-        $nounStrA = $nounA->randomNumber()->nominative();
-        $adjStrA = $adjA->fromNoun($nounA);
-        $verbStr = $verb->past()->personFromNoun($nounA);
+        $adjectives = Adjective::query()
+            ->where('singular_masculine_nominative', '!=', 'который')
+            ->inRandomOrder()
+            ->take(3)
+            ->get()
+            ->map(function ($item) {
+                return (new Word($item))->adjective();
+            })
+            ->all();
 
-        $nounStrB = $nounB->accusative()->randomNumber();
-        $adjStrB = $adjB->fromNoun($nounB);
+        $verbs = Verb::query()
+            ->inRandomOrder()
+            ->take(2)
+            ->get()
+            ->map(function ($item) {
+                return (new Word($item))->verb();
+            })
+            ->all();
 
-        return $adjStrA . ' ' . $nounStrA . ' ' . $verbStr . ' ' . $adjStrB . ' ' . $nounStrB;
+        $which = (new Word(Adjective::query()
+            ->where('singular_masculine_nominative', 'который')
+            ->first()))
+            ->adjective();
+
+        $nounA = $nouns[0]->randomNumber()->nominative();
+        $adjA = $adjectives[0]->fromNoun($nounA);
+        $verbA = $verbs[0]->present()->personFromNoun($nounA);
+
+        $nounB = $nouns[1]->randomNumber()->accusative();
+        $adjB = $adjectives[1]->fromNoun($nounB);
+        $verbB = $verbs[1]->past()->personFromNoun($nounB);
+
+        $nounC = $nouns[2]->randomNumber()->accusative();
+        $adjC = $adjectives[2]->fromNoun($nounC);
+
+        return sprintf('%s %s %s %s %s, %s %s %s %s.', Str::ucfirst($adjA), $nounA, $verbA, $adjB, $nounB,
+            $which->nominative()->numberFromNoun($nounB)->genusFromNoun($nounB), $verbB, $adjC, $nounC);
     }
 }
